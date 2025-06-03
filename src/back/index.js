b3e448ec30275fe280534e1ec105a596bd4a1983
Caio Martins <164074716+Ikaynm@users.noreply.github.com>
2025-06-03 16:29:18 -0300

    att back
    atualizações de estruturas que será utilizada, além da criação da área de criação de produtos dentro do back

---
+++ src/back/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const clienteRoutes = require('./Routes/clienteRoutes');
+const equipamentoRoutes = require('./Routes/equipamentoRoutes');
+app.use(express.json());
+app.use('/cliente', clienteRoutes);
+app.use('/equipamento', equipamentoRoutes);
+
+const PORT = 3010;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+module.exports = { fixwise: app, db };
