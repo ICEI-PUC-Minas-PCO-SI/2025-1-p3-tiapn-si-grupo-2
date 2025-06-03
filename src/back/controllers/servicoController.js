a328136a846d2ad0ae2cdffcfa7312d2e8ba8628
João Vitor Roveda <rovedajoaovitor@gmail.com>
2025-06-03 20:36:52 -0300

    atualizações

--- src/back/controllers/servicoController.js
+++ src/back/controllers/servicoController.js
@@ -1,3 +1,10 @@
 const db = require('../db');
 
 
+exports.CriarServico = (req, res) => {
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).json({ erro: 'Corpo da requisição ausente ou inválido' });
+    }
+
+    
+}
--- src/back/index.js
+++ src/back/index.js
@@ -3,9 +3,11 @@ const app = express();
 const db = require('./db');
 const clienteRoutes = require('./Routes/clienteRoutes');
 const equipamentoRoutes = require('./Routes/equipamentoRoutes');
+const servicoController = require('./Route/servicoController');
 app.use(express.json());
 app.use('/cliente', clienteRoutes);
 app.use('/equipamento', equipamentoRoutes);
+app.use('/servico', servicoController);
 
 const PORT = 3010;
 app.listen(PORT, () => {
