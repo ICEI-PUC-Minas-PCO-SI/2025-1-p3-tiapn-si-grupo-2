abb715ba699b50f17fb502acb3e640845388f16d
João Vitor Roveda <rovedajoaovitor@gmail.com>
2025-06-04 20:02:50 -0300

    iniciação do CRUD de ordem de serviços

---
+++ src/back/Routes/ordemServicoRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const ordemServicoController = require('../controllers/ordemServicoController');
+
+// 👉 Criar Ordem de Serviço
+router.post('/', ordemServicoController.criarOrdemServico);
+
+// 👉 Buscar Ordens de Serviço (com filtros opcionais)
+router.get('/', ordemServicoController.buscarOrdemServico);
+
+// 👉 Atualizar Ordem de Serviço
+router.put('/:idOrdemServico', ordemServicoController.atualizarOrdemServico);
+
+// 👉 Deletar Ordem de Serviço
+router.delete('/:idOrdemServico', ordemServicoController.deletarOrdemServico);
+
+module.exports = router;
